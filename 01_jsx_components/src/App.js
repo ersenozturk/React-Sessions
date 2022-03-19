37aabd65a797df35245638c095e5815edf40be51
ersenozturk <70ozturkersen@gmail.com>
2022-03-19 22:30:46 +0300

    App.js and index.js added

---
+++ 01_jsx_components/src/App.js
@@ -0,0 +1,17 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Msg from "./Msg";
+
+function App() {
+  return (
+    <>
+      <h1>.....Hello React.....</h1>
+      <Header />
+      <Header />
+      <Msg />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
