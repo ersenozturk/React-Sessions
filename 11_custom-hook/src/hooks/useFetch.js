1d9c5f3d3030d3facafe25dbe9ec31e2433103df
ersen <70ozturkersen@gmail.com>
2022-04-10 13:39:45 +0300

    exercises on useFetch.js functions about custom hook

---
+++ 11_custom-hook/src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+function useFetch(url) {
+
+    const [data, setData] = useState()
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
+    
+    useEffect(() => {
+        setLoading(true)
+        fetch(url)
+        .then(response => {
+          if (!response.ok) {
+            setError('Something went wrong!!!')
+            setLoading(false)
+          } else {
+            return response.json()
+          }
+        })
+        .then(data => {
+          setData(data)
+          setLoading(false)
+        });
+    }, [])
+    
+    return {data, loading, error};
+    
+}
+
+export default useFetch;
